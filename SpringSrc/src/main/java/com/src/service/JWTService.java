b618a932d7faa7e31afab96f217a4574871e3b7a
piyush <sab9861@gmail.com>
2024-09-27 17:14:21 +0530

    all done

--- SpringSrc/src/main/java/com/src/service/JWTService.java
+++ SpringSrc/src/main/java/com/src/service/JWTService.java
@@ -36,7 +36,7 @@ public class JWTService {
 				.add(claims)
 				.subject(username)
 				.issuedAt(new Date(System.currentTimeMillis()))
-				.expiration(new Date(System.currentTimeMillis()+600000))
+				.expiration(new Date(System.currentTimeMillis()+60000))
 				.and()
 				.signWith(getKey())
 				.compact();
